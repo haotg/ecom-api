c0ec08bcecfca2b2363332d4ad97632029dd8e34
haotg-ink <jackie@ohmy.ink>
2025-06-19 22:16:43 +0700

    Custom migration

---
+++ prisma/migrations/20250619151207_permission_rename_content_to_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `content` on the `Permission` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Permission" RENAME COLUMN "content" TO "description";
