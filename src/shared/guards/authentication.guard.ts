1b596706d980fd76825057dda6bd6577842ac9ab
haotg-ink <jackie@ohmy.ink>
2025-06-23 18:38:47 +0700

    Refactor Authentication Guard

--- src/shared/guards/authentication.guard.ts
+++ src/shared/guards/authentication.guard.ts
@@ -1,54 +1,76 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, HttpException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
+import { AuthType, ConditionGuard } from 'src/shared/constants/auth.constant'
 import { AUTH_TYPE_KEY, AuthTypeDecoratorPayload } from 'src/shared/decorators/auth.decorator'
 import { AccessTokenGuard } from 'src/shared/guards/access-token.guard'
 import { ApiKeyGuard } from 'src/shared/guards/api-key.guard'
-import { AuthType, ConditionGuard } from '../constants/auth.constant'
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+  private readonly authTypeGuardMap: Record<string, CanActivate>
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
     private readonly apiKeyGuard: ApiKeyGuard,
-  ) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]) ?? { authType: [AuthType.Bearer], options: { condition: ConditionGuard.Or } }
-
-    const authTypeMap = {
+  ) {
+    this.authTypeGuardMap = {
       [AuthType.Bearer]: this.accessTokenGuard,
       [AuthType.APIKey]: this.apiKeyGuard,
       [AuthType.None]: { canActivate: () => true },
     }
-    const guards = authTypeValue.authType.map((authType) => authTypeMap[authType])
-    let error = new UnauthorizedException()
+  }
 
-    if (authTypeValue.options?.condition === ConditionGuard.Or) {
-      for (const guard of guards) {
-        const canActivate = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-        if (canActivate) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const authTypeValue = this.getAuthTypeValue(context)
+    const guards = authTypeValue.authType.map((authType) => this.authTypeGuardMap[authType])
+    return authTypeValue.options?.condition === ConditionGuard.And
+      ? this.handleAndCondition(guards, context)
+      : this.handleOrCondition(guards, context)
+  }
+
+  private getAuthTypeValue(context: ExecutionContext): AuthTypeDecoratorPayload {
+    return (
+      this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? { authType: [AuthType.Bearer], options: { condition: ConditionGuard.And } }
+    )
+  }
+
+  private async handleOrCondition(guards: CanActivate[], context: ExecutionContext) {
+    let lastError: any = null
+
+    // Duyệt qua hết các guard, nếu có 1 guard pass thì return true
+    for (const guard of guards) {
+      try {
+        if (await guard.canActivate(context)) {
           return true
         }
+      } catch (error) {
+        lastError = error
       }
-      throw error
-    } else {
-      for (const guard of guards) {
-        const canActivate = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-        if (!canActivate) {
+    }
+
+    if (lastError instanceof HttpException) {
+      throw lastError
+    }
+    throw new UnauthorizedException()
+  }
+
+  private async handleAndCondition(guards: CanActivate[], context: ExecutionContext) {
+    // Duyệt qua hết các guard, nếu mọi guard đều pass thì return true
+    for (const guard of guards) {
+      try {
+        if (!(await guard.canActivate(context))) {
+          throw new UnauthorizedException()
+        }
+      } catch (error) {
+        if (error instanceof HttpException) {
           throw error
         }
+        throw new UnauthorizedException()
       }
-      return true
     }
+    return true
   }
 }
